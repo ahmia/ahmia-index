0cc1826dc82f069da043f764d6b4afb807f4714d
Stelios <chefarov@gmail.com>
2018-06-07 16:47:30 +0300

    previous attempt to improve point_to_indexes.py was a mess, bring back the old version

--- point_to_indexes.py
+++ point_to_indexes.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 from datetime import datetime, timedelta
 
 import requests
 
+# todo improve the project structure ~ currently even relative imports dont work
 import settings
 
 
@@ -20,44 +23,24 @@ def _index_months(months_ago=0):
 
 def point_to_new_indexes():
     """ Removes any existing alias of latest-crawl, looking at the indexes that
-        correspond to the last 3 months, and creates a new one for the current month
+        correspond to the last 5 months, and creates a new one for the current month
     """
 
     es_aliases_url = "{}_aliases?pretty".format(settings.ES_URL)
+    header = {'Content-Type': 'application/json'}
+    actions_json = {"actions": []}
 
-    # Include current month to avoid a 404 in case the alias points already to current
-    actions_json = {
-        "actions": []
-    }
-    for i in (2, 1, 0):
-        cmd = {
-            "remove": {
-                "index": _index_months(i),
-                "alias": "latest-crawl"
-            }
-        }
-        actions_json["actions"].append(cmd)
+    cmd = {"remove": {"index": _index_months(2), "alias": "latest-crawl"}}
+    actions_json["actions"].append(cmd)
 
-    header = {
-        'Content-Type': 'application/json'
-    }
+    cmd = {"add": {"index": _index_months(1), "alias": "latest-crawl"}}
+    actions_json["actions"].append(cmd)
 
-    print("Removing old latest-crawl alias")
+    cmd = {"add": {"index": _index_months(), "alias": "latest-crawl"}}
+    actions_json["actions"].append(cmd)
+
     resp = requests.post(es_aliases_url, json=actions_json, headers=header)
-    print(resp.text)
-
-    cmd = {
-        "add": {
-            "index": _index_months(0),
-            "alias": "latest-crawl"
-        }
-    }
-    actions_json = {
-        "actions": [cmd]
-    }
-    print("Making new alias: latest-crawl -> {0}".format(_index_months(0)))
-    resp = requests.post(es_aliases_url, json=actions_json, headers=header)
-    print(resp.text)
+    print("{0}\n{1}".format(resp.status_code, resp.text))
 
 
 if __name__ == '__main__':
