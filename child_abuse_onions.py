6f1137c5d55c47dd90141c0904bfe4afdfc4a115
juhanurmi <juha.nurmi@ahmia.fi>
2019-06-19 10:47:50 +0300

    Updated sources for filtering.

--- child_abuse_onions.py
+++ child_abuse_onions.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
+import re
 from bs4 import BeautifulSoup
 # import socks
 # import socket
@@ -19,8 +20,8 @@ from bs4 import BeautifulSoup
 # socket.create_connection = create_connection
 
 
-def get_abuse_onions():
-    html = urlopen("http://uhwikih256ynt57t.onion/wiki/index.php/Hard_Candy")
+def get_abuse_onions_hiddenwiki():
+    html = urlopen("http://mijpsrtgf54l7um6.onion/index.php/Hard_Candy")
     soup = BeautifulSoup(html, "html.parser")
     snippet = soup.find_all('h1')
     urls = []
@@ -33,18 +34,39 @@ def get_abuse_onions():
             while ss.name != "h3":
                 if ss.name == "a" and ss.get('href')[0] != '/':
                     url1 = ss.get('href')[7:29]
+                    url2 = ss.get('href')[7:69]
                     # Add onion domains
-                    if len(url1) == 22 or len(url2) == 62:
-                        if url1[-6:] == ".onion":
-                            urls.append(url1)
-                        if url2[-6:] == ".onion":
-                            urls.append(url2)
+                    if len(url1) == 22 and url1[-6:] == ".onion":
+                        urls.append(url1)
+                    if len(url2) == 62 and url2[-6:] == ".onion":
+                        urls.append(url2)
                 ss = ss.next
-
     urls = list(set(urls))  # Remove duplicates from the list
     for domain in urls:
-        print(domain)
+        if not "mijpsrtgf54l7um6" in domain:
+            print(domain)
 
+def get_abuse_onions_by_url(url):
+    html = urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+    urls = []
+    for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+        address = link.get('href')
+        url1 = address[7:29]
+        url2 = address[7:69]
+        # Add onion domains
+        if len(url1) == 22 and url1[-6:] == ".onion":
+            urls.append(url1)
+        if len(url2) == 62 and url2[-6:] == ".onion":
+            urls.append(url2)
+    urls = list(set(urls))  # Remove duplicates from the list
+    for domain in urls:
+        if not url[7:23] in domain:
+            print(domain)
 
 if __name__ == '__main__':
-    get_abuse_onions()
+    get_abuse_onions_hiddenwiki()
+    url = "http://mijpsrtgf54l7um6.onion/index.php/Underage_erotica"
+    get_abuse_onions_by_url(url)
+    url = "http://toplistftmajmxhb.onion"
+    get_abuse_onions_by_url(url)
