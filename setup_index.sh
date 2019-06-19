79f6e02eea62d02b3e084959e9d4fa060eaee4ff
juhanurmi <juha.nurmi@ahmia.fi>
2019-06-19 02:47:25 -0400

    Handle long new onion address versions in the filter.

--- setup_index.sh
+++ setup_index.sh
@@ -9,7 +9,7 @@ fi
 # Parse the environment variables from .env file, ignoring the comments
 export $(cat .env | grep -v ^# | xargs)
 
-for YEAR in 2018
+for YEAR in 2019 2020 2021
 do
   for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12
   do
