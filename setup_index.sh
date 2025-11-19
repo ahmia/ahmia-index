5d936a47cd53afc748f1d89334bcee7747356a21
juhanurmi <juha.nurmi@ahmia.fi>
2025-11-19 06:42:17 +0200

    New default index.

--- setup_index.sh
+++ setup_index.sh
@@ -13,7 +13,7 @@ for YEAR in {2018..2030}; do
   for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12; do
     
     curl --cacert ${ES_CA_CERTS} --user ${ES_USERNAME}:${ES_PASSWORD} \
-    -XPUT "${ES_URL}$YEAR-$MONTH/" \
+    -XPUT "${ES_URL}onions-$YEAR-$MONTH/" \
     -H 'Content-Type: application/json' -d "@./mappings_tor.json"
 
     echo ""
